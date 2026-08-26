55ae74c170970dadb40ace04d4544b81d3bb1fcd
Parth <parth@Parths-MacBook-Air.local>
2026-08-26 11:46:58 +0530

    chore: add PM2 ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [
+    {
+      name: 'designhub',
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start -H 0.0.0.0 -p 3000',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+        PORT: '3000',
+      },
+    },
+  ],
+};
